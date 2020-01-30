023c0b1bc6fd22941df15d6487b4feee8172ba41
robstat7 <dsankhla2812@gmail.com>
2020-01-31 02:54:03 +0530

    Working LoginChat module

--- src/LoginChat.cpp
+++ src/LoginChat.cpp
@@ -14,48 +14,43 @@ public:
     LoginChat() : PlayerScript("LoginChat") { }
 
     void OnLogin(Player* player) override {
+        if (sConfigMgr->GetBoolDefault("LoginChat.enabled", true))
+        {
+          std::string channelName = sConfigMgr->GetStringDefault("LoginChat.name", "world");
+          QueryResult result = CharacterDatabase.PQuery("SELECT channelId password FROM Channels WHERE name = '%s'", channelName.c_str());
+          if (!result)
+            return;
+          uint32 channelId = (*result)[0].GetUInt32();
+          std::string password = (*result)[1].GetString();
 
-	std::string channelName = sConfigMgr->GetStringDefault("LoginChat.name", "world");
-        QueryResult result = CharacterDatabase.PQuery("SELECT channelId FROM channels WHERE name = '%s'", channelName.c_str());
-
-        if (!result) return;
-        uint32 channelId = (*result)[0].GetUInt32();
-
-    	if (ChannelMgr* cMgr = ChannelMgr::forTeam(player->GetTeamId()))
-    	{
-        	if (Channel* channel = cMgr->GetJoinChannel("world", 9))
-            		channel->JoinChannel(player, "");
-    		cMgr->LoadChannels();
-	}
-
+          if (ChannelMgr* cMgr = ChannelMgr::forTeam(player->GetTeamId()))
+          {
+            if (Channel* channel = cMgr->GetJoinChannel(channelName, channelId))
+              channel->JoinChannel(player, password);
+          }
+        }
     }
 };
 
-class login_chat_conf : public WorldScript
+class LoginChat_Config : public WorldScript
 {
 public:
-    login_chat_conf() : WorldScript("login_chat_conf") { }
+    LoginChat_Config() : WorldScript("LoginChat_Config") { }
 
     void OnBeforeConfigLoad(bool reload) override
     {
         if (!reload) {
             std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/login_chat.conf";
-#ifdef WIN32
-            cfg_file = "reward_shop.conf";
-#endif
+            std::string cfg_file = conf_path + "/LoginChat.conf";
             std::string cfg_def_file = cfg_file + ".dist";
 
             sConfigMgr->LoadMore(cfg_def_file.c_str());
-
             sConfigMgr->LoadMore(cfg_file.c_str());
         }
     }
 };
 
-
 void AddLoginChatScripts() {
-    new LoginChat();
-    new login_chat_conf();
+    new LoginChat;
+    new LoginChat_Config;
 }
-
